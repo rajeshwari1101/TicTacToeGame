e41314cbf3e28b7390434f4f64695332c38993a4
rajeshwari1101 <rajeshwaripatil1101@gmail.com>
2022-03-07 11:30:32 +0530

    Rajeshwari_ImplementingUC1_BoardGame

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Tic Toe Game");
+            TicTacToe obj = new TicTacToe();
+            obj.gameBoard();
+
+        }
+    }
+}
